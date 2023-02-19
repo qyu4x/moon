aa092ed7fcf596c1e3ef3d33c7d0b861eeba02af
Ariq Khoiri <qq.khoiri@gmail.com>
2023-02-19 18:52:50 +0700

    arr of struct

--- c/struct.cpp
+++ c/struct.cpp
@@ -15,6 +15,8 @@ struct Rectangle {
 // define global
 struct Rectangle rectangleOne;
 
+
+const int ARRAY_SIZE =  10;
 int main() {
     // define local directly
     struct  Rectangle rectangleTwo = {10, 12};
@@ -26,12 +28,19 @@ int main() {
     cout<<rectangleTwo.breath<<endl;
 
     // size 12 but reality used 9 byte
-    printf("%lu", sizeof(rectangleTwo));
+    printf("%lu \n", sizeof(rectangleTwo));
 
     // define local
     struct Rectangle rectangleNya;
     rectangleNya.length = 10;
     rectangleNya.breath = 30;
+
+    // define array of structure
+    struct Rectangle arrRectangle[ARRAY_SIZE];
+    arrRectangle[0].length = 10;
+    arrRectangle[0].breath = 20;
+    cout<<arrRectangle[0].length<<endl;
+    cout<<arrRectangle[0].breath<<endl;
 
     return 0;
 
